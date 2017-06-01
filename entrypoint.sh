484fa74433033910aa2396a192d2193d7805ec1d
Julien Chastang <julien.c.chastang@gmail.com>
2017-06-01 15:30:52 +0000

    smarter chowning, I hope

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@ set -e
 
 if [ "$1" = 'runldm.sh' ]; then
 
-    chown -R ldm:ldm ${HOME} && \
-        chown root ${HOME}/bin/ldmd && chmod 4755 ${HOME}/bin/ldmd && \
-        chown root ${HOME}/bin/hupsyslog && chmod 4755 ${HOME}/bin/hupsyslog
+    # don't chown var directory, it takes too long if there is lots of data
+    cd ${HOME} && \
+        chown -R ldm:ldm $(ls -A | awk '{if($1 != "var"){ print $1 }}') && \
+        chown root ./bin/ldmd && chmod 4755 ./bin/ldmd && \
+        chown root ./bin/hupsyslog && chmod 4755 ./bin/hupsyslog
 
     sync
 
