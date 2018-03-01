4ce283b5b544948b4a728d364821ef2b8b8ae697
Julien Chastang <julien.c.chastang@gmail.com>
2018-03-01 11:59:16 -0700

    permissions revamp

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,27 @@ if [ "$1" = 'runldm.sh' ]; then
     groupadd -r ldm -g ${GROUP_ID} && \
     useradd -u ${USER_ID} -g ldm -ms /bin/bash -c "LDM user" ldm
 
-    # don't chown var directory, it takes too long if there is lots of data
-    cd ${HOME} && \
-        chown ldm:ldm . && chown ldm:ldm ./var && chown ldm:ldm ./var/queues && \
-        chown -R ldm:ldm $(ls -A | awk '{if($1 != "var"){ print $1 }}') && \
-        chown ldm:ldm /var/spool/cron/ldm && chmod 600 /var/spool/cron/ldm && \
-        chown root ./bin/ldmd && chmod 4755 ./bin/ldmd && \
-        chown root ./bin/hupsyslog && chmod 4755 ./bin/hupsyslog
+    # All of this is to avoid chowning -R var/data which will take too long if
+    # there is a lot of data
+    for i in /. /var /var/data /var/queues /var/logs
+    do
+        j=${HOME}$i
+        if [ -d "$j" ]; then
+            chown ldm:ldm "$j"
+        fi
+    done
+
+    # chown everything in ${HOME} except var/data which will take too long
+    cd ${HOME} && chown -R ldm:ldm $(ls -A | awk '{if($1 != "var"){ print $1 }}')
+
+    # deal with cron for user ldm
+    if [ -f /var/spool/cron/ldm ]; then
+        chown ldm:ldm /var/spool/cron/ldm && chmod 600 /var/spool/cron/ldm
+    fi
+
+    chown root ${HOME}/bin/hupsyslog ${HOME}/bin/ldmd
+
+    chmod 4755 ${HOME}/bin/hupsyslog ${HOME}/bin/ldmd
 
     sync
 
@@ -26,4 +40,3 @@ if [ "$1" = 'runldm.sh' ]; then
 fi
 
 exec "$@"
-
