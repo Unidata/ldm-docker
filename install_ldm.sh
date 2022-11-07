7d1e1d4d08f68587a977c7d337ebee3ea5ad63e8
B. Espinoza <respinoza@ucar.edu>
2022-11-07 10:31:09 -0700

    Update fetching of source code; Log to file and stdout

--- install_ldm.sh
+++ install_ldm.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-wget https://www.unidata.ucar.edu/downloads/ldm/ldm-${LDM_VERSION}.tar.gz
+# wget https://www.unidata.ucar.edu/downloads/ldm/ldm-${LDM_VERSION}.tar.gz
+wget ftp://ftp.unidata.ucar.edu/pub/ldm/ldm-${LDM_VERSION}.tar.gz
 
 gunzip -c ldm-${LDM_VERSION}.tar.gz | pax -r '-s:/:/src/:'
 
@@ -10,9 +11,9 @@ rm ldm-${LDM_VERSION}.tar.gz
 
 cd /home/ldm/ldm-${LDM_VERSION}/src
 
-./configure --disable-root-actions
+./configure --disable-root-actions 2>&1 | tee configure.log
 
-make install > make.log 2>&1
+make install 2>&1 | tee make.log
 
 # optional
 # make distclean
